54b0588b2b62c57ece59f25a5937573db441ede5
VickyMutai <vicky.mutai96@gmail.com>
2018-12-17 17:32:52 +0300

    ensure service worker is registered an all pages

--- js/indexController.js
+++ js/indexController.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('sw.js', { scope: './' }).then((reg) => {
-        console.log("SW registered")
+      navigator.serviceWorker.register('sw.js', { scope: '/' }).then((reg) => {
+        console.log("SW registered. Scope is "+reg.scope);
       }).catch( (err) => {
         // reg failed :(
         console.error(`SW failed: ${err}`);
