21a62b758fbd07d4f28903c1fc411b9ab7cd41aa
VickyMutai <vicky.mutai96@gmail.com>
2018-12-23 18:14:58 +0300

    caching all resources

--- sw.js
+++ sw.js
@@ -1,25 +1,35 @@
-let cacheName='resturant-review-v1';
-let urlsToCache=[
-    '/',
-    'css/',
-    'data/',
-    'img/',
-    'js/',
-    'restaurant.html',
-];
-function cacheAssets(){
-    return caches.open(cacheName).then(cache => {
-        return cache.addAll(urlsToCache);
-    });
-}
-function updateCache(){
-    return caches.keys().then(function(cacheName){
-        return Promise.all(
-            cacheName.map(function(cache){
-                if (cacheName.indexOf(cache) === -1){
-                    return caches.delete(cache);
-                }
-            })
-        );
-    });
-}
+self.addEventListener('install',function(event){
+    event.waitUntil(
+        caches.open('restaurant-review-v1').then(function(cache){
+            return cache.addAll([
+                'index.html',
+                'restaurant.html',
+                'img/1.jpg',
+                'img/2.jpg',
+                'img/3.jpg',
+                'img/4.jpg',
+                'img/5.jpg',
+                'img/6.jpg',
+                'img/7.jpg',
+                'img/8.jpg',
+                'img/9.jpg',
+                'img/10.jpg',
+                'js/main.js',
+                'data/',
+                'js/restaurant_info.js',
+                'js/dbhelper.js',
+                'js/A11yhelper.js',
+                'js/indexController.js',
+                'css/styles.css',
+            ]);
+        })
+    );
+});
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+          if(response) return response;
+          return fetch(event.request);  
+        })
+    );   
+});
