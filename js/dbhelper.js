b27b28ae1e2804d63c17cf1078b413f3c7efe081
VickyMutai <vicky.mutai96@gmail.com>
2019-01-10 14:24:14 +0300

    add idb file

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -17,20 +17,17 @@ class DBHelper {
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+    fetch(DBHelper.DATABASE_URL)
+    .then(response => response.json())
+    .then(function(jsonResponse) {
+      callback(null, jsonResponse);
+    })
+    .catch(function(error) {
+      const errorMessage = (`Request failed. Returned status of ${error}`);
+      callback(errorMessage, null);
+    });
   }
+
 
   /**
    * Fetch a restaurant by its ID.
@@ -147,12 +144,21 @@ class DBHelper {
     return (`./restaurant.html?id=${restaurant.id}`);
   }
 
-  /**
+
+    /**
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}.jpg`);
   }
+
+  /**
+   * Restaurant image alt.
+   */
+  static imageAltForRestaurant(restaurant) {
+    return (`${restaurant.alt_text}`);
+  }
+
 
   /**
    * Map marker for a restaurant.
--- sw.js
+++ sw.js
@@ -1,30 +1,36 @@
-let staticCacheName = 'restaurant-review-v1';
-let URLsToCache = [
-    'index.html',
-    'restaurant.html',
-    'img/1.jpg',
-    'img/2.jpg',
-    'img/3.jpg',
-    'img/4.jpg',
-    'img/5.jpg',
-    'img/6.jpg',
-    'img/7.jpg',
-    'img/8.jpg',
-    'img/9.jpg',
-    'img/10.jpg',
-    'js/main.js',
-    'data/',
-    'js/restaurant_info.js',
-    'js/dbhelper.js',
-    'js/A11yhelper.js',
-    'js/indexController.js',
-    'css/styles.css',
-];
-
 self.addEventListener('install',function(event){
     event.waitUntil(
-        caches.open(staticCacheName).then(function(cache){
-            return cache.add(URLsToCache);
+        caches.open('restaurant-review-v1').then(function(cache){
+            return cache.addAll( [
+                './',
+                './index.html',
+                './restaurant.html',
+                './sw.js',
+                './css/styles.css',
+                './img/1.jpg',
+                './img/2.jpg',
+                './img/3.jpg',
+                './img/4.jpg',
+                './img/5.jpg',
+                './img/6.jpg',
+                './img/7.jpg',
+                './img/8.jpg',
+                './img/9.jpg',
+                './img/10.jpg',
+                // './img/marker-icon-2x-red.png',
+                // './img/marker-shadow.png',
+                // './js/idb.js',
+                './js/dbhelper.js',
+                './js/indexController.js',
+                // './js/bouncemarker.js',
+                './js/main.js',
+                './js/restaurant_info.js',
+                'http://localhost:1337/restaurants/',
+                'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+                'https://fonts.googleapis.com/css?family=Open+Sans:300,400',
+                'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+            ]
+            );
             console.log("working")
         })
     );
@@ -42,7 +48,7 @@ self.addEventListener('activate',function(event){
         caches.keys().then(function(cacheNames){
             return Promise.all(
                 cacheNames.filter(function(cacheName){
-                    return cacheName.startsWith('restaurant-')&& cacheName != staticCacheName;
+                    return cacheName.startsWith('restaurant-')&& cacheName != 'restaurant-review-v1';
                 }).map(function(cacheName){
                     return cache.delete(cacheName);
                 })
